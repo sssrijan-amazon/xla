e47e3587ec95ce2314d30486c824263187329548
manfeibaigithub <manfei@google.com>
2024-03-29 17:52:59 +0000

    test formal change

--- test/test_fori_loop_with_while_loop_simple_add_dispatch_in_torch.py
+++ test/test_fori_loop_with_while_loop_simple_add_dispatch_in_torch.py
@@ -42,7 +42,7 @@ class WhileLoopTest(unittest.TestCase):
 
     init = torch.tensor([10], dtype=torch.int32, device=device)
     limit_value = torch.tensor([0], dtype=torch.int32, device=device)
-    res = while_loop(cond_fn, body_fn, init, limit_value) # (init, limit_value))
+    res = while_loop(cond_fn, body_fn, (init, limit_value))
     expected = _fake_while_loop(cond_fn, body_fn, (init, limit_value))
     self.assertEqual(expected, res)
 
@@ -62,7 +62,7 @@ class WhileLoopTest(unittest.TestCase):
     # TODO(@manfei): init and limit_value has to be torch.tensor.
     init = torch.tensor([0], dtype=torch.int32, device=device)
     limit_value = torch.tensor([10], dtype=torch.int32, device=device)
-    res = while_loop(cond_fn, body_fn, init, limit_value) # (init, limit_value))
+    res = while_loop(cond_fn, body_fn, (init, limit_value))
     expected = _fake_while_loop(cond_fn, body_fn, (init, limit_value))
     self.assertEqual(expected, res)
 
